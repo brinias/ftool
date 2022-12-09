f964d60229d3ae93d5e8c218b68c0b550d9d0260
brinias <101641065+brinias@users.noreply.github.com>
2022-12-09 12:54:21 +0200

    Add files via upload

---
+++ at.py
@@ -0,0 +1,26 @@
+import google.auth.transport.requests
+import requests
+from google.oauth2 import service_account
+from google.auth.transport.requests import AuthorizedSession
+
+# Define the required scopes
+scopes = [
+  "https://www.googleapis.com/auth/userinfo.email",
+  "https://www.googleapis.com/auth/firebase.database"
+]
+
+# Authenticate a credential with the service account
+credentials = service_account.Credentials.from_service_account_file(
+    "/home/brinias2/key.json", scopes=scopes)
+
+# Use the credentials object to authenticate a Requests session.
+authed_session = AuthorizedSession(credentials)
+response = authed_session.get(
+    "https://ftool-2ebc1-default-rtdb.firebaseio.com/users/ada/name.json")
+
+# Or, use the token directly, as described in the "Authenticate with an
+# access token" section below. (not recommended)
+request = google.auth.transport.requests.Request()
+credentials.refresh(request)
+access_token = credentials.token
+print access_token
